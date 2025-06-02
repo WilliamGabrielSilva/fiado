920fffe46af9a010376b0039b9d2dc3559264a57
William Gabriel Silva <127068960+WilliamGabrielSilva@users.noreply.github.com>
2025-06-02 13:51:33 -0300

    hfgh

--- app.js
+++
@@ -1,7 +0,0 @@
-import {supabase} from './supabaseClient.js'
-
-async function getUser() {
-  const { data: { user } } = await supabase.auth.getUser()
-  if (!user) window.location.href = 'login.html'
-  return user
-}
