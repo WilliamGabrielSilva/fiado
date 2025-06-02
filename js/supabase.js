4b182b73046c1b2665a8a109492f66b0be79b4c6
William Gabriel Silva <127068960+WilliamGabrielSilva@users.noreply.github.com>
2025-06-02 14:15:32 -0300

    asdsa

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,3 @@
 const supabaseUrl = 'https://pgqrjtglzibalkjlkrxt.supabase.co';
-const supabaseKey = 'SUA_PUBLIC_ANON_KEY_AQUI'; // Proteja isso em produção
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBncXJqdGdsemliYWxramxrcnh0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzY5NDY2MSwiZXhwIjoyMDYzMjcwNjYxfQ.tlYQRgXgxpQ8YrpaP-1_9vOB547K_ZH23HkEr2g92oI'; // Proteja isso em produção
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
