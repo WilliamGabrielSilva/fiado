5cefd0138e06d0b4a4c6b86b5c595f6c1cb3fda2
William Gabriel Silva <127068960+WilliamGabrielSilva@users.noreply.github.com>
2025-06-02 14:35:34 -0300

    adasdsa

--- js/supabase.js
+++
@@ -1,3 +0,0 @@
-const supabaseUrl = 'https://pgqrjtglzibalkjlkrxt.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBncXJqdGdsemliYWxramxrcnh0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzY5NDY2MSwiZXhwIjoyMDYzMjcwNjYxfQ.tlYQRgXgxpQ8YrpaP-1_9vOB547K_ZH23HkEr2g92oI'; // Proteja isso em produção
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
--- js/ui.js
+++
@@ -1,7 +0,0 @@
-function abrirModalDetalhes(clienteId, nomeCliente) {
-  console.log('Abrir detalhes para', clienteId);
-}
-
-function fecharModal() {
-  document.getElementById('modalDetalhes').style.display = 'none';
-}
