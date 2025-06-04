d8d017cf7ec12a7bdc3186b4d5b46b641fd3e446
William Gabriel Silva <127068960+WilliamGabrielSilva@users.noreply.github.com>
2025-06-04 12:02:21 -0300

    dasdaS

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
 const CACHE_NAME = 'meu-pwa-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/app.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/index.html'
 ];
 
 // Instalar Service Worker
